9a8f172400f2014b79b488995d3a15875be9577f
ykoitzsch <y.koitzsch@gmx.at>
2022-03-23 20:20:09 +0100

    initial commit

---
+++ apps/gitter-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gitter-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'gitter-app';
+}
